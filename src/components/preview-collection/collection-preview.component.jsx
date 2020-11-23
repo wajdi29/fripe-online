3be586317cfd613fba22705c7f0b4211ee293b5f
wajdi29 <wajdidouiri26@gmail.com>
2020-11-23 09:18:23 +0100

    collection styling

--- src/components/preview-collection/collection-preview.component.jsx
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './preview-collection.styles.scss'
+import CollectionItem from '../collection-item/collection-item.component'
 
 const CollectionPreview = ({title, items}) => (
     <div className="collection-preview"> 
@@ -8,8 +9,8 @@ const CollectionPreview = ({title, items}) => (
 {
 items
 .filter((item, index) => index < 4)
-.map((item) => (
-    <div key={item.id}>{item.name}</div>
+.map(({id, ...otherItemProps}) => (
+    <CollectionItem key={id} {...otherItemProps} />
 ))
 }
     </div>

2e5af5a1f377a8bec1a43f4482c4fca0cce08e65
wajdi29 <wajdidouiri26@gmail.com>
2020-11-24 12:12:38 +0100

    Sign in and sign out

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './sign-in.styles.scss'
 import FormInput from '../form-input/form-input.component'
 import CustomButton from '../custom-button/custom-button.component'
-
+import { signInWithGoogle } from '../../firebase/firebase.utils'
 class SignIn extends React.Component {
     constructor(props) {
         super(props);
@@ -42,6 +42,8 @@ class SignIn extends React.Component {
                     label="password"
                     required />
                 <CustomButton type="submit">Sign In</CustomButton>
+                <CustomButton onClick={signInWithGoogle}>Sign In with Goolge</CustomButton>
+
             </form>
         </div>)
     }
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -19,9 +19,9 @@ const config = {
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
 
-  const provider = new firebase.auth.GoogleAuthProvider();
+ const provider = new firebase.auth.GoogleAuthProvider();
   provider.setCustomParameters({prompt: 'select_account'});
 
-  export const SignInWithGoogle = () => auth.signInWithPopup(provider);
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
   export default firebase;

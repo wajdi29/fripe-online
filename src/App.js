4e30259200f39c55b9d5e81c9f22d53222c4c1a4
wajdi29 <wajdidouiri26@gmail.com>
2020-11-25 19:10:03 +0100

    storing users in cloud firestore

--- src/App.js
+++ src/App.js
@@ -1,46 +1,45 @@
 import React from "react"
 import './App.css';
-import { Route ,Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component'
-import  ShopPage from './pages/shop/shop.component'
+import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
-import {auth} from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
       currentUser: null
+    }
   }
-}
 
-unsubscribeFromAuth = null
+  unsubscribeFromAuth = null
 
-componentDidMount() {
-  this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-    this.setState({currentUser: user})
-    console.log(user)
-  })
-}
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+      createUserProfileDocument(user)
+    })
+  }
 
-componentWillUnmount() {
-  this.unsubscribeFromAuth()
-}
+  componentWillUnmount() {
+    this.unsubscribeFromAuth()
+  }
 
 
   render() {
     return (
       <div>
-      <Header currentUser={this.state.currentUser}/>
-      <Switch>
-      <Route exact path='/' component={HomePage}/>
-      <Route path='/shop' component={ShopPage}/>
-      <Route path='/signin' component={SignInAndSignUpPage}/>
-      </Switch>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
       </div>
     );
   }
-  
+
 };
 
 export default App;

de63bd2ff59e88f895bcb38102049df48fc01a86
wajdi29 <wajdidouiri26@gmail.com>
2020-11-26 18:10:07 +0100

    finishing sing in handle submitcomponent

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './sign-in.styles.scss'
 import FormInput from '../form-input/form-input.component'
 import CustomButton from '../custom-button/custom-button.component'
-import { signInWithGoogle } from '../../firebase/firebase.utils'
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils'
 class SignIn extends React.Component {
     constructor(props) {
         super(props);
@@ -14,8 +14,14 @@ class SignIn extends React.Component {
 
 
     }
-    handleSubmit = (e) => {
+    handleSubmit = async (e) => {
         e.preventDefault()
+        const { email, password } = this.state
+        try {
+            await auth.signInWithEmailAndPassword(email, password)
+        } catch (error) {
+            console.log(error)
+        }
         this.setState({ email: '', password: '' })
     };
 

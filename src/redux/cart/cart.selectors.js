15a77b9c7f430008e289576b2c6b29f82588a2ef
wajdi29 <wajdidouiri26@gmail.com>
2020-11-30 09:03:26 +0100

    added checkoutpage

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -16,3 +16,10 @@ export const selectCartItemsCount = createSelector(
     cartItems => cartItems.reduce((accumaltedQuantity, cartItem) => accumaltedQuantity + cartItem.quantity,
         0)
 );
+export const selectCartTotal = createSelector(
+ [selectCartItems], 
+cartItems => cartItems
+ .reduce((accumaltedQuantity, cartItem) => accumaltedQuantity
+ + cartItem.quantity * cartItem.price,
+    0)
+    )

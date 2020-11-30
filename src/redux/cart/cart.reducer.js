f86a40558ddd968848d181fc4331018a56694bd6
wajdi29 <wajdidouiri26@gmail.com>
2020-11-30 16:28:52 +0100

    removing items from cartitem feature

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types'
-import { addItemToCart } from './cart.utils'
+import { addItemToCart, removeItemFromCart } from './cart.utils'
 const INITIAL_STATE = {
     hidden: true,
     cartItems: []
@@ -26,6 +26,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
 
                 )
 
+            }
+        case CartActionTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
             }
 
         default:

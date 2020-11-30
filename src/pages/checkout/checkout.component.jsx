4b86986238beb9215285ee9c474af51717482159
wajdi29 <wajdidouiri26@gmail.com>
2020-11-30 15:15:13 +0100

    created remove button

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,49 +1,51 @@
 
 import React from 'react'
 import './checkout.styles.scss'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
-import  CheckoutItem from '../../components/checkout-item/checkout-item.component'
+import CheckoutItem from '../../components/checkout-item/checkout-item.component'
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
 
-const CheckoutPage = ({cartItems, total}) => (
+const CheckoutPage = ({ cartItems, total }) => (
     <div className="checkout-page">
 
-    <div className='checkout-header'>
+        <div className='checkout-header'>
 
-    <div className='header-blocks'>
-    <span>Product</span>
+            <div className='header-blocks'>
+                <span>Product</span>
+            </div>
+
+            <div className='header-blocks'>
+                <span>Description</span>
+            </div>
+
+            <div className='header-blocks'>
+                <span>Quantity</span>
+            </div>
+
+            <div className='header-blocks'>
+                <span>Price</span>
+            </div>
+
+            <div className='header-blocks'>
+                <span>Remove</span>
+            </div>
+
+        </div>
+        {
+            cartItems.map(cartItem => (
+
+                <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+            ))
+
+        }
+
+        <div className='total'>
+            <span>TOTAL: ${total}</span>
+        </div>
+
     </div>
 
-    <div className='header-blocks'>
-    <span>Description</span>
-    </div>
-
-    <div className='header-blocks'>
-    <span>Quantity</span>
-    </div>
-
-    <div className='header-blocks'>
-    <span>Price</span>
-    </div>
-
-    <div className='header-blocks'>
-    <span>Remove</span>
-    </div>
-
-    </div>
-    {
-       cartItems.map(cartItem => (
-           <CheckoutItem key={cartItem.id} cartItem={cartItems} />
-       ) ) 
-    }
-
-    <div className='total'>
-    <span>TOTAL: ${total}</span>
-    </div>
-
-    </div>
-    
 )
 
 const mapStateToProps = createStructuredSelector({

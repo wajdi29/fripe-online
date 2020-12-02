102f9b37ecad416953f3f842a0fc4f9e605516eb
wajdi29 <wajdidouiri26@gmail.com>
2020-12-02 20:34:04 +0100

    ckeckout page routing

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-import  CollectionPage from '../collection/collection.component'
+import CollectionPage from '../collection/collection.component'
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
 const ShopPage = ({ match }) => (
 
     <div className='shop-page'>
-        <Route exact path={`${match.pat}`} component ={ CollectionsOverview } />
-        <Route path={`${match.path}/:collectionId`} component={ CollectionPage}/>
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
 )
 

ba9e8c1e8b24bc3c648589b24274d34b8c3a8b26
wajdi29 <wajdidouiri26@gmail.com>
2020-12-03 12:44:26 +0100

    setting up stripe payment

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -5,7 +5,7 @@ import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 import CheckoutItem from '../../components/checkout-item/checkout-item.component'
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
-
+import StripeCheckoutButton from '../../components/stripe-button/stripe-componenet.component'
 const CheckoutPage = ({ cartItems, total }) => (
     <div className="checkout-page">
 
@@ -40,10 +40,14 @@ const CheckoutPage = ({ cartItems, total }) => (
 
         }
 
-        <div className='total'>
-            <span>TOTAL: ${total}</span>
-        </div>
-
+        <div className='total'> <span>TOTAL: ${total}</span></div>
+               <div className='test-warning'>
+               *Please user the following test credit card for payment
+               <br/>
+              4242 4242 4242 4242 - Exp: 01/21 - CVV: 123
+               </div>
+    
+            <StripeCheckoutButton price={total} />
     </div>
 
 )
